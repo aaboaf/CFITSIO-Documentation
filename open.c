8db0f8774e1905b41269c9ac28c97ca62e82719e
aaboaf <aaboaf@uchicago.edu>
2017-07-19 13:29:40 -0500

    Add open.c
    Demonstrates opening a fits file using fits_open_file.

---
+++ open.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdio.h>
+#include "fitsio.h"
+
+int main(int argc, char *argv[])
+{
+	fitsfile *fptr;
+	//sets up a fitsfile pointer to store the header
+	int status = 0, nkeys, ii; //initializes status and creates vars for the number of keys
+				 //and an iterator
+	fits_open_file(&fptr, argv[1], READONLY, &status); //Opens the file described in argv[1]
+							   //at the fptr pointer as read only. 
+	if(status)//checks if an error occurs (status not equal to zero)
+		fits_report_error(stderr, status); //reports error
+	return status;
+}
