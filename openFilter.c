2d7f6657a3a30fa601ceeac5837065426c033767
aaboaf <aaboaf@uchicago.edu>
2017-07-19 13:50:11 -0500

    Update openFilter.c

--- openFilter.c
+++ openFilter.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[])
 {
         fitsfile *fptr;
 
-        int status = 0, nkeys, ii;
+        int status = 0;
 char* str = strcat(argv[1], "[col FILTERNAME]"); //Filtername is the name of the filter. This specific code will filter out a column FILTERNAME
         fits_open_file(&fptr, str, READONLY, &status);
         if(status)
